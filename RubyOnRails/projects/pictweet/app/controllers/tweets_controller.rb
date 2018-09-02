23a11afdb8166d9b18ec4ccb04288a3c05ad599c
KodyBird <torikura.k@gmail.com>
2018-09-02 20:55:10 +0900

    login ,signeup

--- RubyOnRails/projects/pictweet/app/controllers/tweets_controller.rb
+++ RubyOnRails/projects/pictweet/app/controllers/tweets_controller.rb
@@ -1,7 +1,8 @@
 class TweetsController < ApplicationController
 
+    before_action :move_to_index, except: :index
     def index
-        @tweets = Tweet.all.order("created_at DESC").page(params[:page]).per(5)
+        @tweets = Tweet.page(params[:page]).per(5).order("created_at DESC")
     end
     def new
     end
@@ -13,5 +14,9 @@ class TweetsController < ApplicationController
     private
     def tweet_params
       params.permit(:name, :image, :text)
+    end
+
+    def move_to_index
+        redirect_to action: :index unless user_signed_in?
     end
  end
